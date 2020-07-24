9eccac3fe754fe810f2f84e657d8e60faf327aeb
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-07-24 11:32:10 +0900

    ci: add unit test step

--- src/md2html.ts
+++ src/md2html.ts
@@ -1,5 +1,6 @@
 import marked from "marked";
 
+// todo: defines a type for cliOptions
 export function md2html(md: string, cliOptions: any): string {
   return marked(md, {
     gfm: cliOptions.gfm,
