26ddd07e4fddae6285d0071bcaa1d0748103312d
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-07-24 09:35:12 +0900

    add: fs to read files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import commander from "commander";
+import fs from "fs";
 
 commander.parse(process.argv);
-console.log("command:", commander);
+
+const filePath = commander.args[0];
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+  if (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+  console.log(file);
+});
