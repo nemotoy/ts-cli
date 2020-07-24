0e8b84bbd72c2f653c4346018d564ce833c45bf5
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-07-24 10:21:19 +0900

    add: default option of gfm

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,23 @@ import commander from "commander";
 import fs from "fs";
 import marked from "marked";
 
+commander.option("--gfm", "enable GFM");
 commander.parse(process.argv);
-
 const filePath = commander.args[0];
+
+const cliOptions = {
+  gfm: false,
+  ...commander.opts(),
+};
+
 fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
   if (err) {
     console.error(err.message);
     process.exit(1);
   }
-  const html = marked(file);
+  const html = marked(file, {
+    gfm: cliOptions.gfm,
+  });
+
   console.log(html);
 });
